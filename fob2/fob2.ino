0d00bd71425e21d98dc29aa470bef682310e0555
Pete Hodgson <git@thepete.net>
2014-12-26 15:37:41 -0800

    successful TOTP support

--- fob2/fob2.ino
+++ fob2/fob2.ino
@@ -4,15 +4,26 @@
 #include <Adafruit_SSD1306.h>
 #include <Time.h>
 
-#define OLED_RESET 4
-Adafruit_SSD1306 display(OLED_RESET);
+#include "sha1.h"
+#include "TOTP.h"
 
 #if (SSD1306_LCDHEIGHT != 32)
 #error("Height incorrect, please fix Adafruit_SSD1306.h!");
 #endif
 
+#define OLED_RESET_PIN 4
+#define PUSH_BUTTON_PIN 3
+
+// The shared secret is MyLegoDoor
+uint8_t hmacKey[] = {0x4d, 0x79, 0x4c, 0x65, 0x67, 0x6f, 0x44, 0x6f, 0x6f, 0x72};
+TOTP totp = TOTP(hmacKey, 10);
+
+
+Adafruit_SSD1306 display(OLED_RESET_PIN);
+
 void setup() {                
   Serial.begin(9600);
+  pinMode(PUSH_BUTTON_PIN, INPUT);
 
   // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x32)
@@ -28,15 +39,31 @@ void setup() {
 }
 
 void loop() {
+  loopWithoutDelay(); 
+  delay(100);
+}
+
+void loopWithoutDelay(){
   checkForTimeSync();
   
   if( timeStatus() == timeNotSet ){
     displayTimeSyncNeeded();
-  }else{
-    writeTimeToDisplay();
+    return;
   }
   
-  delay(100);
+  if( digitalRead(PUSH_BUTTON_PIN) ){
+    writeTimeToDisplay();
+  }else{
+    blankDisplay();
+  }
+  
+  String totpCode = totp.getCode(now());
+  Serial.println( "TOTP: "+totpCode )
+}
+  
+void blankDisplay(){
+  display.clearDisplay();
+  display.display();
 }
 
 void displayTimeSyncNeeded(){
@@ -78,6 +105,7 @@ void checkForTimeSync(){
   display.setCursor(0,0);
   display.println("SYNCED!");
   display.display();
+  delay(500);
 }
 
 void writeTimeToDisplay(){
